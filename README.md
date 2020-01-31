19ba01c74dec2cfb0b4cbfa9b9cf0f0b45c0743e
amerre <adr.merre@gmail.com>
2020-01-31 14:17:53 +0100

    algorithm building

---
+++ README.md
@@ -0,0 +1,16 @@
+# Download sorter
+
+A tool that allows you to sort your downloaded files in directories. This is my first python script that I built for myself in order to apply what I've just learned.
+Feel free to notify me if you find something that I can improve.
+
+## Built With
+
+- [Python](https://www.python.org/)
+
+## Run this project
+
+In the [testing_files](../testing_files) directory I've put empty files in order to test rapidly the script. All you need to do is to move the [testing_files](../testing_files)'s content in the root of the project, and run the script. Normally,directories must be created and everything should be sorted immediately.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import os
-import shutil
 from os import path
 
-# ------------------------------
+# -----------Functions-----------
 # Create directories
-def create_dir(directory, parent_directory = "/Users/Adrien/dev/python"):
+def create_dir(directory, parent_directory = "/Users/Adrien/dev/python/Download-organizer"):
     try:
         path = os.path.join(parent_directory, directory)
         os.mkdir(path)
-        print("Directory "+ directory +" has been created!")
+        print("Directory '" + directory + "' has been created!")
     except FileExistsError:
         print("Files already exist")
 # ------------------------------
 
+
 # Files
 files = [f for f in os.listdir() if path.isfile(f)]
 # Directories
-dir = [d for d in os.listdir() if path.isdir(d)]
+directories = [d for d in os.listdir() if path.isdir(d)]
 
 # Array where files are gonna be dispatched
 filesList = {
@@ -27,18 +27,29 @@ filesList = {
     "executables": []
 }
 
-# Where we distinguish files between themselves
+# Where we distinguish files between themselves and sort them in the array
 for file in files:
     extension = file.split(".",1)[1]
     # Vidéos (avi, mkv, mp4...)
-    if  extension == "avi" or "mkv" or "mp4" or "mov":
-        filesList["video"].append(file)
+    if extension == "avi" or extension == "mkv" or extension == "mp4" or extension == "mov":
+        filesList["videos"].append(file)
     # Text based documents (pdf, doc, txt...)
-    elif  extension == "pdf" or "doc" or "txt" or "odf":
+    elif extension == "pdf" or extension == "doc" or extension == "txt" or extension == "odf":
         filesList["documents"].append(file)
     # Compressed documents (zip, rar...)
-    elif  extension == "zip" or "rar":
+    elif  extension == "zip" or extension == "rar":
         filesList["compressed"].append(file)
+    # Executables (exe...)
+    elif  extension == "png" or extension == "jpg":
+        filesList["photos"].append(file)
+    # Executables (exe...)
+    elif  extension == "exe":
+        filesList["executables"].append(file)
 
-print(filesList)
-    
+# Created directories that we need in function or the files
+for fileType in filesList:
+    if filesList[fileType]:
+        create_dir(fileType)
+        for file in filesList[fileType]:
+            os.rename("/Users/Adrien/dev/python/Download-organizer/" + file, "/Users/Adrien/dev/python/Download-organizer/" + fileType + "/" + file)
+            print(file + " has been sorted in the " + fileType + " directory")
