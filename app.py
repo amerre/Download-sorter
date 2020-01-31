025e94e7f1a152cf566b3cf24594464a1a72c298
amerre <adr.merre@gmail.com>
2020-01-31 15:06:27 +0100

    add directory support

--- app.py
+++ app.py
@@ -12,46 +12,51 @@ def create_dir(directory, parent_directory = os.getcwd()):
         print("Files already exist")
 # ------------------------------
 
-
 # Files
 files = [f for f in os.listdir() if path.isfile(f)]
 # Directories
 directories = [d for d in os.listdir() if path.isdir(d)]
 
 # Array where files are gonna be dispatched
-filesList = {
+toSort = {
     "videos": [],
     "documents": [],
     "compressed": [],
     "photos": [],
-    "executables": []
+    "executables": [],
+    "directories": []
 }
 
 # Where we distinguish files between themselves and sort them in the array
 for file in files:
-    extension = file.split(".",1)[1]
+    extension = file.rsplit(".",1)[1]
     # Vidéos (avi, mkv, mp4...)
     if extension == "avi" or extension == "mkv" or extension == "mp4" or extension == "mov":
-        filesList["videos"].append(file)
+        toSort["videos"].append(file)
     # Text based documents (pdf, doc, txt...)
     elif extension == "pdf" or extension == "doc" or extension == "txt" or extension == "odf":
-        filesList["documents"].append(file)
+        toSort["documents"].append(file)
     # Compressed documents (zip, rar...)
     elif  extension == "zip" or extension == "rar":
-        filesList["compressed"].append(file)
+        toSort["compressed"].append(file)
     # Executables (exe...)
     elif  extension == "png" or extension == "jpg":
-        filesList["photos"].append(file)
+        toSort["photos"].append(file)
     # Executables (exe...)
     elif  extension == "exe":
-        filesList["executables"].append(file)
+        toSort["executables"].append(file)
+
+# Where we distinguish directories and sort them in the array
+for directory in directories:
+    if directory != "testing_files" and directory != ".git" and directory != "videos" and directory != "documents" and directory != "compressed" and directory != "photos" and directory != "executables" and directory != "directories":
+        toSort["directories"].append(directory)
 
 
 # Created directories that we need in function or the files
-for fileType in filesList:
-    if filesList[fileType]:
+for fileType in toSort:
+    if toSort[fileType]:
         create_dir(fileType)
-        for file in filesList[fileType]:
+        for file in toSort[fileType]:
             os.rename(os.getcwd() + "/" + file, os.getcwd() + "/" + fileType + "/" + file)
             print(os.getcwd() + "/" + file)
             print(file + " has been sorted in the " + fileType + " directory")
